53e101ad4d3bd3f5b08f6dd2ae17a8f9516363ea
publius2016 <manuelsalcido2012@gmail.com>
2017-08-15 15:19:42 -0500

    Refactored app.js ticker display code

--- views/assets/js/app.js
+++ views/assets/js/app.js
@@ -4,122 +4,30 @@ BITFINEX TICKER WEB SOCKET DISPLAY
 
 var socket = io.connect('/');
 
-socket.on('btc', function (data) {
-  // console.log('BTC: ', data.message.LAST_PRICE);
+function tickerDisplay(currency) {
+  socket.on(currency, (data) => {
+    currencyID = "#"+ currency;
+    $(currencyID + " span").empty();
 
-  $("#btc span").empty();
+    $(currencyID + " .last_price").append(data.message.LAST_PRICE);
+    $(currencyID + " .low").append(data.message.LOW);
+    $(currencyID + " .high").append(data.message.HIGH);
+    $(currencyID + " .vol").append(data.message.VOLUME);
+    $(currencyID + " .chg").append(data.message.DAILY_CHANGE);
+    $(currencyID + " .percChg").append(data.message.DAILY_CHANGE_PERC * 100);
 
-  $("#btc .last_price").append(data.message.LAST_PRICE);
-  $("#btc .low").append(data.message.LOW);
-  $("#btc .high").append(data.message.HIGH);
-  $("#btc .vol").append(data.message.VOLUME);
-  $("#btc .chg").append(data.message.DAILY_CHANGE);
-  $("#btc .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
+  });
+}; // END TICKERDISPLAY FUNCTION
 
-socket.on('ltc', function (data) {
-  // console.log('LTC: ', data.message.LAST_PRICE);
-
-  $("#ltc span").empty();
-
-  $("#ltc .last_price").append(data.message.LAST_PRICE);
-  $("#ltc .low").append(data.message.LOW);
-  $("#ltc .high").append(data.message.HIGH);
-  $("#ltc .vol").append(data.message.VOLUME);
-  $("#ltc .chg").append(data.message.DAILY_CHANGE);
-  $("#ltc .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
-
-socket.on('eth', function (data) {
-  // console.log('ETH: ', data.message.LAST_PRICE);
-
-  $("#eth span").empty();
-
-  $("#eth .last_price").append(data.message.LAST_PRICE);
-  $("#eth .low").append(data.message.LOW);
-  $("#eth .high").append(data.message.HIGH);
-  $("#eth .vol").append(data.message.VOLUME);
-  $("#eth .chg").append(data.message.DAILY_CHANGE);
-  $("#eth .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
-
-socket.on('iot', function (data) {
-  // console.log('IOT: ', data.message.LAST_PRICE);
-
-  $("#iot span").empty();
-
-  $("#iot .last_price").append(data.message.LAST_PRICE);
-  $("#iot .low").append(data.message.LOW);
-  $("#iot .high").append(data.message.HIGH);
-  $("#iot .vol").append(data.message.VOLUME);
-  $("#iot .chg").append(data.message.DAILY_CHANGE);
-  $("#iot .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
-
-socket.on('etc', function (data) {
-  // console.log('ETC: ', data.message.LAST_PRICE);
-
-  $("#etc span").empty();
-
-  $("#etc .last_price").append(data.message.LAST_PRICE);
-  $("#etc .low").append(data.message.LOW);
-  $("#etc .high").append(data.message.HIGH);
-  $("#etc .vol").append(data.message.VOLUME);
-  $("#etc .chg").append(data.message.DAILY_CHANGE);
-  $("#etc .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
-
-socket.on('dsh', function (data) {
-  // console.log('DSH: ', data.message.LAST_PRICE);
-
-  $("#dsh span").empty();
-
-  $("#dsh .last_price").append(data.message.LAST_PRICE);
-  $("#dsh .low").append(data.message.LOW);
-  $("#dsh .high").append(data.message.HIGH);
-  $("#dsh .vol").append(data.message.VOLUME);
-  $("#dsh .chg").append(data.message.DAILY_CHANGE);
-  $("#dsh .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
-
-socket.on('xrp', function (data) {
-  // console.log('XRP: ', data.message.LAST_PRICE);
-
-  $("#xrp span").empty();
-
-  $("#xrp .last_price").append(data.message.LAST_PRICE);
-  $("#xrp .low").append(data.message.LOW);
-  $("#xrp .high").append(data.message.HIGH);
-  $("#xrp .vol").append(data.message.VOLUME);
-  $("#xrp .chg").append(data.message.DAILY_CHANGE);
-  $("#xrp .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
-
-socket.on('bcc', function (data) {
-  // console.log('BCC: ', data.message.LAST_PRICE);
-
-  $("#bcc span").empty();
-
-  $("#bcc .last_price").append(data.message.LAST_PRICE);
-  $("#bcc .low").append(data.message.LOW);
-  $("#bcc .high").append(data.message.HIGH);
-  $("#bcc .vol").append(data.message.VOLUME);
-  $("#bcc .chg").append(data.message.DAILY_CHANGE);
-  $("#bcc .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
-
-socket.on('xmr', function (data) {
-  // console.log('XMR: ', data.message.LAST_PRICE);
-
-  $("#xmr span").empty();
-
-  $("#xmr .last_price").append(data.message.LAST_PRICE);
-  $("#xmr .low").append(data.message.LOW);
-  $("#xmr .high").append(data.message.HIGH);
-  $("#xmr .vol").append(data.message.VOLUME);
-  $("#xmr .chg").append(data.message.DAILY_CHANGE);
-  $("#xmr .percChg").append(data.message.DAILY_CHANGE_PERC);
-});
+tickerDisplay("btc");
+tickerDisplay("ltc");
+tickerDisplay("eth");
+tickerDisplay("iot");
+tickerDisplay("etc");
+tickerDisplay("dsh");
+tickerDisplay("xrp");
+tickerDisplay("bcc");
+tickerDisplay("xmr");
 
 
 /******************************************************************
